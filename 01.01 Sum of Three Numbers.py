073d95be00a6f1e3c88bb0583ede50efdfe538cb
annakae <akedwards@ualr.edu>
2022-08-24 16:15:01 +0000

    Sum  of Three Numbers

--- 01.01 Sum of Three Numbers.py
+++ 01.01 Sum of Three Numbers.py
@@ -1,8 +1,21 @@
 a = input("First Number: ")
 b = input("Second Number: ")
 c = input("Third Number: ")
-
 d = int(a) + int(b) + int(c)
-print(d)2
-3
+print(d)
+
 print ("")
+
+a = input("First Number: ")
+b = input("Second Number: ")
+c = input("Third Number: ")
+d = int(b) + int(a) + int(c)
+print(d)
+
+print ("")
+
+a = input("First Number: ")
+b = input("Second Number: ")
+c = input("Third Number: ")
+d = int(a) + int(b) + int(c)
+print(d)
