2a208d958ee7f9294b13d3f8974f88e07155b82e
annakae <akedwards@ualr.edu>
2022-09-13 17:07:00 +0000

    Tens Digit

--- 2.03 Swap Digits.py
+++
@@ -1,7 +0,0 @@
-x = int(input("Enter a Number: "))
-y = x // 10
-z = x % 10
-
-n = (z*10) + y
-
-print("Swapped Number: {}" .format(n))
--- 2.05 Tens Digit.py
+++
@@ -1,4 +0,0 @@
-x = int(input("Enter a Number: "))
-y = (x % 100) // 10
-
-print("Tens Digit: {}".format(y))
