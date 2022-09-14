0e4409d3eb801d60e7b8a9f30acc7379b9f53c7d
annakae <akedwards@ualr.edu>
2022-09-14 02:15:56 +0000

    Days in Month

--- 03.08 Days In Month.py
+++ 03.08 Days In Month.py
@@ -4,5 +4,7 @@ if x in {1, 3, 5, 7, 8, 10, 12} :
     print("31")
 elif x in {4, 6, 9, 11} :
     print("30")
-else:
+elif x in {2} :
     print("28")
+else:
+    print("Please enter an integer between 1-12")
