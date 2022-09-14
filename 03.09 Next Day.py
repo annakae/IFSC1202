5e1bf81425ba2e1e9beb47263a0a748d997e1f07
annakae <akedwards@ualr.edu>
2022-09-14 03:05:21 +0000

    not finished

---
+++ 03.09 Next Day.py
@@ -0,0 +1,20 @@
+x = int(input("Enter Month: "))
+
+if x in {1, 3, 5, 7, 8, 10, 12} :
+    month = 31
+elif x in {4, 6, 9, 11} :
+    month = 30
+elif x in {2} :
+    month = 28
+
+y = int(input("Enter Day: "))
+
+if y < month :
+    y = y + 1
+else:
+    y = 1
+
+if y < month :
+    month 
+
+print("Next Day: {}/{}" .format(x, y))
