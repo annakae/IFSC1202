21faf9a163c5e1a0fa7755bb0ed3ca437cb2d436
annakae <akedwards@ualr.edu>
2022-09-20 21:27:03 +0000

    Sum of Sequence

--- 05.01 Sum of Sequence.py
+++ 05.01 Sum of Sequence.py
@@ -1,1 +1,9 @@
-int (input("Enter a Number (zero to quit)"))
+sum = 0
+
+while True:
+    n = int(input("Enter a Number (zero to quit): "))
+    sum += n
+    if n == 0: 
+        break
+
+print("Sum:", sum)
--- HelloWorld.py
+++ HelloWorld.py
@@ -1,14 +1,9 @@
-result = 0
-debt = 1000
-prod = 1
-divine = 100
-n = 5
-for i in range(1, n + 1):
-    result += i   # the same as result = result + i
-    debt -= i     # the same as debt = debt - i
-    prod *= i     # the same as prod = prod * i
-    divine /= i   # the same as divine = divine / i
-print(result)
-print(debt)
-print(prod)
-print(divine)
+sum = 0
+
+while True:
+    n = int(input("Enter a Number (zero to quit): "))
+    sum += n
+    if n == 0: 
+        break
+
+print("Sum:", sum)
