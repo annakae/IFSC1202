3dabb93784d062298f5d48e58519ee6877811846
annakae <akedwards@ualr.edu>
2022-09-20 18:51:40 +0000

    Product of N Numbers

--- 04.01 Product of N Numbers.py
+++ 04.01 Product of N Numbers.py
@@ -1,7 +1,7 @@
 n = int (input("Enter N: "))
 result = 1
 
-for i in range(n):
+for i in range(1, n+1):
     int(input("Enter Number: "))
     result = result * i
 
---
+++ 04.02 Sum of Cubes.py
@@ -0,0 +1,5 @@
+x = int(input("Enter Number: "))
+y = 0
+for i in range(1, x+1):
+    y = y + pow(i,3)
+print(y)
