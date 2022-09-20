c67f87ce517ada11b423eb8bccc9741490a8a7eb
annakae <akedwards@ualr.edu>
2022-09-20 22:46:58 +0000

    Fibonacci

--- 05.09 Fibonacci Numbers.py
+++ 05.09 Fibonacci Numbers.py
@@ -1,1 +1,14 @@
-f = int(input("Enter Fibonnaci Sequence Number: "))
+def Fibonacci(n):
+    if n <1:
+        return n
+
+    if n == 0:
+        return 0
+
+    if n == 1 or n == 2:
+        return 1
+
+    return ((Fibonacci(n-1)) + (Fibonacci(n-2)))
+    
+n = int(input("Enter Fibonacci Sequence Number: "))
+print("Fibonacci Number:", Fibonacci(n))
