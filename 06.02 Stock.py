ba86b96fece520c3084b77c4bd35b7f22f8b5cdc
annakae <akedwards@ualr.edu>
2022-10-05 14:27:58 +0000

    Stock

---
+++ 06.02 Stock.py
@@ -0,0 +1,12 @@
+file = open("06.02 Stock.txt","r")
+
+data = file.read()
+data = (float(i) for i in data.split())
+
+print("Price       Change")
+
+print(data(0))
+
+for i in range(1,len(data)):
+    percent = ((data(i) - data(i - 1) / data(i - 1) * 100))
+    print("{:.2f}    {:+.2f}%".format(data(i),percent))
