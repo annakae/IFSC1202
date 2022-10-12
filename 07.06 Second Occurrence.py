a9bddb51303431685af6745782f3d317706816db
annakae <akedwards@ualr.edu>
2022-10-12 16:17:12 +0000

    Second Occurrence

---
+++ 07.06 Second Occurrence.py
@@ -0,0 +1,11 @@
+s = input("Enter a string: ")
+
+index = s.find('f')
+if index == -1:
+    print("Zero f")
+else:
+    index = s.find('f', index + 1)
+    if index == -1:
+        print("One f")
+    else:
+        print(index)
