44a7efc3d844bdfcd2bd62b1151836c8e98ca7ef
annakae <akedwards@ualr.edu>
2022-10-17 19:22:14 +0000

    Unique Elements

---
+++ 08.10 Number of Unique Elements.py
@@ -0,0 +1,14 @@
+inputlist = list()
+input = input("Enter Values Seperated by Spaces: " ).split()
+n = len(input)
+
+for i in range(n):
+    inputlist.append(int(input[i]))
+
+for i in range(n):
+    count = 1
+    for j in range(n):
+        if inputlist[i] == inputlist[j] and i != j:
+            count += 1
+    if count == 1:
+        print(inputlist[i])
