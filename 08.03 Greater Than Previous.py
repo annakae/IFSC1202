2983de493e706d0284ce3317a78d0c9ebe6dabd6
annakae <akedwards@ualr.edu>
2022-10-17 18:31:08 +0000

    Greater than Previous

---
+++ 08.03 Greater Than Previous.py
@@ -0,0 +1,7 @@
+values = input("Enter Values Seperated by Spaces: ")
+lst = values.split(' ')
+
+for i in range(0,len(lst)):
+    if i<len(lst)-1:
+        if int(lst[i+1])>int(lst[i]):
+            print(int(lst[i+1]))
