a728b7f1fda8dcf35b8c4fcb8f0d149670a1d005
annakae <akedwards@ualr.edu>
2022-10-17 18:56:29 +0000

    Neighbors Same Sign

---
+++ 08.04 Neighbors Of The Same Sign.py
@@ -0,0 +1,24 @@
+s = input()
+my_list_str = s.split()
+my_list = []
+
+
+for beta in my_list_str:
+    my_list.append(int(beta))
+
+
+found = False
+for i in range(len(my_list) - 1):
+  if my_list[i] > 0 and my_list[i + 1] > 0:
+     print (my_list[i], end =' ')
+     print (my_list[i + 1])
+     found = True
+     break
+  elif my_list[i] < 0 and my_list[i + 1] < 0:     
+      print (my_list[i], end =' ')
+      print (my_list[i + 1])
+      found = True
+      break
+    
+if not found:
+    print ('NONE')
